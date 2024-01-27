e8be837c2bfe6d684db1c8d64d1ac5cd2a7b49d0
bruno <bruno@users-MacBook-Air.local>
2024-01-27 17:52:54 -0300

    45. Mastering Admin template

--- routes/admin.php
+++ routes/admin.php
@@ -2,7 +2,7 @@
 
 Route::group(['middleware' => ['role:admin']], function () {
     Route::get('/', function(){
-        echo 'this is admin section';
+        return view('layouts.backend');
     });
 });
 
