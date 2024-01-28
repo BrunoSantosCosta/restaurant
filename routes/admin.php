359696059b825f7b4829e72b4dea009380adbfa9
bruno <bruno@users-MacBook-Air.local>
2024-01-28 09:39:47 -0300

    46. Category create and store

--- routes/admin.php
+++ routes/admin.php
@@ -4,5 +4,7 @@ Route::group(['middleware' => ['role:admin']], function () {
     Route::get('/', function(){
         return view('layouts.backend');
     });
+
+    Route::resource('/category',  \App\Http\Controllers\CategoryController::class, ['names' => 'category']);
 });
 
