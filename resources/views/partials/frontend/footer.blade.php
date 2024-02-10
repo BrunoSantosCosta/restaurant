8a519beac954c2b443efdcf745b3c29803b18f71
bruno <bruno@users-MacBook-Air.local>
2024-02-10 07:30:02 -0300

    58. Mastering template for the home page

---
+++ resources/views/partials/frontend/footer.blade.php
@@ -0,0 +1,55 @@
+
+<footer>
+		<div class="container">
+			<div class="row justify-content-between">
+				<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+					<div class="footer_wp">
+						<i class="icon_pin_alt"></i>
+						<h3>Address</h3>
+						<p>7232 Broadway Suite 308, Jackson Heights, 11372, NY, United States</p>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+					<div class="footer_wp">
+						<i class="icon_tag_alt"></i>
+						<h3>Reservations</h3>
+						<p>
+							<a href="tel:009442323221">+94 423-23-221 </a>
+							<a href="tel:009442323221">+55 548-58-998 </a>
+							<a href="#0">reservations@Foores.com</a>
+						</p>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+					<h3>Keep in touch</h3>
+					<div id="newsletter">
+						<div id="message-newsletter"></div>
+						<form method="post"
+							action="http://www.ansonika.com/foores/phpmailer/newsletter_template_email.php"
+							name="newsletter_form" id="newsletter_form">
+							<div class="form-group">
+								<input type="email" name="email_newsletter" id="email_newsletter" class="form-control"
+									placeholder="Your email">
+								<button type="submit" id="submit-newsletter"><i class="arrow_carrot-right"></i></button>
+							</div>
+						</form>
+						<div class="follow_us">
+							<ul>
+								<li><a href="#0"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="#0"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#0"><i class="fab fa-linkedin-in"></i></a></li>
+								<li><a href="#0"><i class="fab fa-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<hr>
+			<div class="row">
+				<div class="col-12 text-center">
+					<p class="copy">© Foores Restaurant - All rights reserved</p>
+				</div>
+			</div>
+			<p class="text-center"></p>
+		</div>
+	</footer>
